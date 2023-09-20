c4be593ce1d7f6148f766dffa3a90277dece8663
AlonaVasylets <alonavasylets@gmail.com>
2023-09-20 12:31:39 +0300

    homework 41 changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import './App.css';
 
+const schema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  email: yup.string().required('Email is required').email('Email is not valid'),
+  phone: yup
+    .string()
+    .required('Phone is required')
+    .matches(/^\d{12}$/, 'The phone number must consist of 12 digits'),
+});
+
 function App() {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data) => {
-    data.name = data.name.replace(/\s+/g, ' ').trim();
-    console.log(data);
+    const newData = {...data};
+    newData.name = newData.name.replace(/\s+/g, ' ').trim();
+    console.log(newData);
     reset();
   }
 
@@ -16,46 +30,23 @@ function App() {
       <form className='container' onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>Name:</label>
-          <input
-            type='text'
-            name='name'
-            {...register('name', { required: true })} />
+          <input type='text' name='name' {...register('name')} />
           {errors.name && (
-            <p className='errMsg'>Name is required</p>
-          )
-
-          }
+            <p className='errMsg'>{errors.name.message}</p>
+          )}
         </div>
         <div>
           <label>Email:</label>
-          <input
-            type='text'
-            name='email'
-            {...register('email', {
-              required: true,
-              pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-            })} />
-          {errors.email?.type === 'required' && (
-            <p className='errMsg'>Email is required</p>
-          )}
-          {errors.email?.type === 'pattern' && (
-            <p className='errMsg'>Email is not valid</p>
+          <input type='text' name='email' {...register('email')} />
+          {errors.email && (
+            <p className='errMsg'>{errors.email.message}</p>
           )}
         </div>
         <div>
           <label>Phone:</label>
-          <input
-            type='text'
-            name='phone'
-            {...register('phone', {
-              required: true,
-              pattern: /^[0-9]{12}$/,
-            })} />
-          {errors.phone?.type === 'required' && (
-            <p className='errMsg'>Phone is required</p>
-          )}
-          {errors.phone?.type === 'pattern' && (
-            <p className='errMsg'>The phone number must consist of 12 digits</p>
+          <input type='text' name='phone' {...register('phone')} />
+          {errors.phone && (
+            <p className='errMsg'>{errors.phone.message}</p>
           )}
         </div>
         <button type='submit'>Send</button>
